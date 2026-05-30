94144f4db2f19572a687aa4eb204d06a8f3b5d9d
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2026-05-30 19:29:15 -0400

    feature/ef-26: Add CloudWatch dashboard, DLQ alarms, and Lambda error alarms

--- infra/lib/eventflow-stack.ts
+++ infra/lib/eventflow-stack.ts
@@ -7,7 +7,9 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as pythonLambda from "@aws-cdk/aws-lambda-python-alpha";
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
 import { Construct } from "constructs";
+import { error } from "node:console";
 
 export class EventFlowStack extends cdk.Stack {
   public readonly bus: events.EventBus;
@@ -391,6 +393,206 @@ export class EventFlowStack extends cdk.Stack {
     paymentHandler.addEnvironment(
       "PAYMENT_SECRET_ARN",
       paymentSecret.secretArn,
+    );
+
+    const dashboard = new cloudwatch.Dashboard(this, "EventFlowDashboard", {
+      dashboardName: "EventFlow-Metrics-Dashboard",
+    });
+
+    dashboard.addWidgets(
+      new cloudwatch.GraphWidget({
+        title: "Lambda Invocations",
+        left: [
+          orderHandler.metricInvocations(),
+          inventoryHandler.metricInvocations(),
+          paymentHandler.metricInvocations(),
+          notificationHandler.metricInvocations(),
+        ],
+      }),
+      new cloudwatch.GraphWidget({
+        title: "Lambda Errors",
+        left: [
+          orderHandler.metricErrors(),
+          inventoryHandler.metricErrors(),
+          paymentHandler.metricErrors(),
+          notificationHandler.metricErrors(),
+        ],
+      }),
+      new cloudwatch.GraphWidget({
+        title: "Lambda Duration",
+        left: [
+          orderHandler.metricDuration(),
+          inventoryHandler.metricDuration(),
+          paymentHandler.metricDuration(),
+          notificationHandler.metricDuration(),
+        ],
+      }),
+      new cloudwatch.GraphWidget({
+        title: "SQS Queue Depth",
+        left: [
+          this.orderQueue.metricApproximateNumberOfMessagesVisible(),
+          this.inventoryQueue.metricApproximateNumberOfMessagesVisible(),
+          this.paymentQueue.metricApproximateNumberOfMessagesVisible(),
+          this.notificationQueue.metricApproximateNumberOfMessagesVisible(),
+        ],
+      }),
+      new cloudwatch.GraphWidget({
+        title: "SQS Dead Letter Queue Depth",
+        left: [
+          orderDlq.metricApproximateNumberOfMessagesVisible(),
+          inventoryDlq.metricApproximateNumberOfMessagesVisible(),
+          paymentDlq.metricApproximateNumberOfMessagesVisible(),
+          notificationDlq.metricApproximateNumberOfMessagesVisible(),
+        ],
+      }),
+    );
+
+    const orderDlqAlarm = new cloudwatch.Alarm(this, "OrderDlqAlarm", {
+      metric: orderDlq.metricApproximateNumberOfMessagesVisible(),
+      threshold: 0,
+      evaluationPeriods: 1,
+      comparisonOperator:
+        cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+    });
+
+    const inventoryDlqAlarm = new cloudwatch.Alarm(this, "InventoryDlqAlarm", {
+      metric: inventoryDlq.metricApproximateNumberOfMessagesVisible(),
+      threshold: 0,
+      evaluationPeriods: 1,
+      comparisonOperator:
+        cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+    });
+
+    const paymentDlqAlarm = new cloudwatch.Alarm(this, "PaymentDlqAlarm", {
+      metric: paymentDlq.metricApproximateNumberOfMessagesVisible(),
+      threshold: 0,
+      evaluationPeriods: 1,
+      comparisonOperator:
+        cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+    });
+
+    const notificationDlqAlarm = new cloudwatch.Alarm(
+      this,
+      "NotificationDlqAlarm",
+      {
+        metric: notificationDlq.metricApproximateNumberOfMessagesVisible(),
+        threshold: 0,
+        evaluationPeriods: 1,
+        comparisonOperator:
+          cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      },
+    );
+
+    const allDlQAlarms = [
+      orderDlqAlarm,
+      inventoryDlqAlarm,
+      paymentDlqAlarm,
+      notificationDlqAlarm,
+    ];
+
+    const anyDlqCompositeAlarm = new cloudwatch.CompositeAlarm(
+      this,
+      "AnyDlqCompositeAlarm",
+      {
+        alarmRule: cloudwatch.AlarmRule.anyOf(...allDlQAlarms),
+      },
+    );
+
+    dashboard.addWidgets(
+      new cloudwatch.AlarmWidget({
+        title: "DLQ Alarm Status",
+        alarm: anyDlqCompositeAlarm,
+      }),
+    );
+
+    const orderHandlerErrorAlarm = new cloudwatch.Alarm(
+      this,
+      "OrderHandlerErrorAlarm",
+      {
+        metric: orderHandler.metricErrors({
+          period: cdk.Duration.minutes(5),
+          statistic: "Sum",
+        }),
+        threshold: 1,
+        evaluationPeriods: 1,
+        comparisonOperator:
+          cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        alarmName: "eventflow-order-handler-errors",
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      },
+    );
+
+    const inventoryHandlerErrorAlarm = new cloudwatch.Alarm(
+      this,
+      "InventoryHandlerErrorAlarm",
+      {
+        metric: inventoryHandler.metricErrors({
+          period: cdk.Duration.minutes(5),
+          statistic: "Sum",
+        }),
+        threshold: 1,
+        evaluationPeriods: 1,
+        comparisonOperator:
+          cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        alarmName: "eventflow-inventory-handler-errors",
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      },
+    );
+
+    const paymentHandlerErrorAlarm = new cloudwatch.Alarm(
+      this,
+      "PaymentHandlerErrorAlarm",
+      {
+        metric: paymentHandler.metricErrors({
+          period: cdk.Duration.minutes(5),
+          statistic: "Sum",
+        }),
+        threshold: 1,
+        evaluationPeriods: 1,
+        comparisonOperator:
+          cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        alarmName: "eventflow-payment-handler-errors",
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      },
+    );
+
+    const notificationHandlerErrorAlarm = new cloudwatch.Alarm(
+      this,
+      "NotificationHandlerErrorAlarm",
+      {
+        metric: notificationHandler.metricErrors({
+          period: cdk.Duration.minutes(5),
+          statistic: "Sum",
+        }),
+        threshold: 1,
+        evaluationPeriods: 1,
+        comparisonOperator:
+          cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        alarmName: "eventflow-notification-handler-errors",
+        treatMissingData: cloudwatch.TreatMissingData.NOT_BREACHING,
+      },
+    );
+
+    const allLambdaErrorAlarms = [
+      orderHandlerErrorAlarm,
+      inventoryHandlerErrorAlarm,
+      paymentHandlerErrorAlarm,
+      notificationHandlerErrorAlarm,
+    ];
+
+    const anyLambdaErrorCompositeAlarm = new cloudwatch.CompositeAlarm(
+      this,
+      "AnyLambdaErrorCompositeAlarm",
+      {
+        alarmRule: cloudwatch.AlarmRule.anyOf(...allLambdaErrorAlarms),
+      },
+    );
+
+    dashboard.addWidgets(
+      new cloudwatch.AlarmWidget({
+        title: "Lambda Error Alarm Status",
+        alarm: anyLambdaErrorCompositeAlarm,
+      }),
     );
   }
 }
